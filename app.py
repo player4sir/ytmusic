e152a7ef57723ce88d7d7f83aee81f0ef3b7a422
我是海军呀 <31569037+player4sir@users.noreply.github.com>
2024-05-04 16:38:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,33 +17,5 @@ def search_song():
     response = [{'id': result['videoId'], 'title': result['title'], 'artist': result['artists'][0]['name']} for result in results]
     return jsonify(response)
 
-@app.route('/getstream', methods=['GET'])
-def get_stream():
-    song_id = request.args.get('songid')
-    song_url = f"https://music.youtube.com/watch?v={song_id}"
-    try:
-        url = ''
-        ydl_opts = {
-            'youtube_skip_dash_manifest': True,
-            'outtmpl': '%(title)s.%(ext)s',
-            'quiet': True,
-            'no_warnings': True,
-            'extractaudio': True,
-            'audioformat': 'mp3',
-            'preferredcodec': 'mp3',
-            'nocheckcertificate': True,
-            'format_limit': 1,
-            'max_downloads': 1,
-                    }
-
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(song_url, download=False)
-            url = info['formats'][0]['url']
-        return jsonify({'stream_url': url})
-    except:
-        yt = YouTube(f'https://www.youtube.com/watch?v={song_id}')
-        url = yt.streams.filter(only_audio=True).first().url
-        return jsonify({'stream_url': url})
-
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0")
