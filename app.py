621e99e88de4f2cd3ebe55a21c08dfef69131943
我是海军呀 <31569037+player4sir@users.noreply.github.com>
2024-06-25 04:18:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,22 @@ import json
 from flask import Flask, request, jsonify
 from ytmusicapi import YTMusic
 import os
+
 app = Flask(__name__)
 yt = YTMusic(auth=json.loads(os.environ.get('OAUTH_JSON_CONTENT', '{}')))
+
 @app.route('/search', methods=['GET'])
 def search_song():
     keyword = request.args.get('keyword')
     results = yt.search(keyword, filter='songs', limit=30)
-    response = [{'id': result['videoId'], 'title': result['title'], 'artist': result['artists'][0]['name']} for result in results]
+    response = []
+    for result in results:
+        if 'artists' in result and len(result['artists']) > 0:
+            response.append({
+                'id': result.get('videoId', ''),
+                'title': result.get('title', ''),
+                'artist': result['artists'][0].get('name', '')
+            })
     return jsonify(response)
 
 if __name__ == "__main__":
